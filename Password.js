f0712529d8b8038315a84716cf42d0dde7e49648
Samarth-agr <143349851+Samarth-agr@users.noreply.github.com>
2024-03-28 12:30:16 +0530

    Update Password.js

--- Password.js
+++ Password.js
@@ -5,28 +5,32 @@ const Symbols = '!@#$%^&*'
 let allowchar = "123456789";
 
 function GenerateRandomPassword() {
-    let length = document.getElementById("length").value
-    let Lower = document.getElementById("AllowLower").checked
-    let upper = document.getElementById("AllowUpper").checked
-    let symbol = document.getElementById("AllowSymbols").checked  
-    let password = ""
+    let length = document.getElementById("length").value;
+    let Lower = document.getElementById("AllowLower").checked;
+    let upper = document.getElementById("AllowUpper").checked;
+    let symbol = document.getElementById("AllowSymbols").checked;
+    let password = "";
 
     if (length <= 4) {
-        inner.innerHTML = '<h2>The Length Must be atleast 4</h2>'
-        return 0
+        inner.innerHTML = '<h2>The Length Must be at least 4</h2>';
+        return;
     }
-    allowchar += Lower ? LowerCaseChar : "";
-    allowchar += upper ? UpperCaseChar : "";
-    allowchar += symbol ? Symbols : "";
 
-    if (allowchar.length == 0) {
-        inner.innerHTML = '<h3>Atleast one box must be checked </h3>'
-        return 0;
+    let allowchar = "123456789";
+    allowchar += Lower ? 'abcdefghijklmnopqrstuvwxyz' : "";
+    allowchar += upper ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : "";
+    allowchar += symbol ? '!@#$%^&*' : "";
+
+    if (allowchar.length === 0) {
+        inner.innerHTML = '<h3>At least one box must be checked</h3>';
+        return;
     }
+
     for (let i = 0; i < length; i++) {
-        let random = Math.floor(Math.random() * allowchar.length)
+        let random = Math.floor(Math.random() * allowchar.length);
         password += allowchar[random];
     }
-    inner.innerHTML = `<h2>Your Generated Password : ${password}</h2>`
-    return 0;
+
+    inner.innerHTML = `<h2>Your Generated Password: ${password}</h2>`;
 }
+
